64baba0f4e613abfb2f615ff34c2464c89312832
svdegron <svdegron@gmail.com>
2023-03-25 03:19:52 +0500

    Add files
    Add all files from lesson 5 with update One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -2,8 +2,12 @@
 // Этот файл точка входа
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
 	public static void main(String[] args) {
-		Two.greeting();
+		// Two.greeting();
+		
+		System.out.println(StringUtils.reverse("Hexlet"));
 	}
 }
